ba8565a30fa6e1796410c44e9da9d1181cfd6acf
DigiWiz87 <53604844+DigiWiz87@users.noreply.github.com>
2022-09-08 23:48:09 +0100

    Add files via upload

---
+++ software/portal_gun.ino
@@ -0,0 +1,78 @@
+#include <Adafruit_NeoPixel.h>
+#ifdef __AVR__
+ #include <avr/power.h> // Required for 16 MHz Adafruit Trinket
+#endif
+
+// Which pin on the Arduino is connected to the NeoPixels?
+// On a Trinket or Gemma we suggest changing this to 1:
+#define LED_PIN    6
+
+// How many NeoPixels are attached to the Arduino?
+#define LED_COUNT 8
+int delayval = 500;
+
+// Declare our NeoPixel strip object:
+Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
+// Argument 1 = Number of pixels in NeoPixel strip
+// Argument 2 = Arduino pin number (most are valid)
+// Argument 3 = Pixel type flags, add together as needed:
+//   NEO_KHZ800  800 KHz bitstream (most NeoPixel products w/WS2812 LEDs)
+//   NEO_KHZ400  400 KHz (classic 'v1' (not v2) FLORA pixels, WS2811 drivers)
+//   NEO_GRB     Pixels are wired for GRB bitstream (most NeoPixel products)
+//   NEO_RGB     Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
+//   NEO_RGBW    Pixels are wired for RGBW bitstream (NeoPixel RGBW products)
+const int BUTTON_PIN = 3; // the number of the pushbutton pin
+
+// Variables will change:
+int lastState = HIGH; // the previous state from the input pin
+int currentState;    // the current reading from the input pin
+
+void setup() {
+  // initialize serial communication at 9600 bits per second:
+  #if defined(__AVR_ATtiny85__) && (F_CPU == 16000000)
+  clock_prescale_set(clock_div_1);
+#endif
+  Serial.begin(9600);
+  // initialize the pushbutton pin as an pull-up input
+  // the pull-up input pin will be HIGH when the switch is open and LOW when the switch is closed.
+  pinMode(BUTTON_PIN, INPUT_PULLUP);
+   strip.begin();           // INITIALIZE NeoPixel strip object (REQUIRED)
+  strip.show();            // Turn OFF all pixels ASAP
+  strip.setBrightness(50); // Set BRIGHTNESS to about 1/5 (max = 255)
+}
+
+void loop() {
+   // Fill along the length of the strip in various colors...
+   currentState = digitalRead(BUTTON_PIN);
+
+  //if(currentState == LOW)
+   //colorWipe(strip.Color(253,   102,   0), 50); // Blue
+   //if(currentState == HIGH)// Red
+  //colorWipe(strip.Color(  0, 120,   255), 50); // Orange
+
+  if(currentState == LOW)
+   colorWipe(strip.Color(171,   5,   5), 50); // Red 
+   if(currentState == HIGH)// Red
+  colorWipe(strip.Color(  251, 242,   41), 50); // Yellow
+
+   //if(currentState == LOW)
+  // colorWipe(strip.Color(253,   102,   0), 50); // Purple
+  // if(currentState == HIGH)// Red
+  //colorWipe(strip.Color(  0, 120,   255), 50); // Cyan
+}
+
+
+// Some functions of our own for creating animated effects -----------------
+
+// Fill strip pixels one after another with a color. Strip is NOT cleared
+// first; anything there will be covered pixel by pixel. Pass in color
+// (as a single 'packed' 32-bit value, which you can get by calling
+// strip.Color(red, green, blue) as shown in the loop() function above),
+// and a delay time (in milliseconds) between pixels.
+void colorWipe(uint32_t color, int wait) {
+  for(int i=0; i<strip.numPixels(); i++) { // For each pixel in strip...
+    strip.setPixelColor(i, color);         //  Set pixel's color (in RAM)
+    strip.show();                          //  Update strip to match
+    delay(wait);                           //  Pause for a moment
+  }
+}
