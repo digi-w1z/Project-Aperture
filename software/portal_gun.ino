e29519aed883dd8daee014106c128f14dc5caf69
DigiWiz87 <53604844+DigiWiz87@users.noreply.github.com>
2022-12-15 10:41:06 +0000

    Update portal_gun.ino

--- software/portal_gun.ino
+++ software/portal_gun.ino
@@ -8,21 +8,24 @@
 const unsigned long eventInterval = 300000;
 unsigned long previousTime = 0;
 
-#define NUM_LEDS 24 
-#define PIN 7 
+#define NUM_LEDS 24
+#define PIN 7
 #define LED  8
 #define LED_2 13
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUM_LEDS, PIN, NEO_GRB + NEO_KHZ800);
 
-#define BUTTON 12
-#define BUTTON_2 11
+#define BUTTON 11
+#define BUTTON_2 12
 
-static const uint8_t PIN_MP3_TX = 2; // Connects to module's RX 
-static const uint8_t PIN_MP3_RX = 3; // Connects to module's TX 
+bool oldState = LOW;
+int showType = 0;
+
+static const uint8_t PIN_MP3_TX = 2; // Connects to module's RX
+static const uint8_t PIN_MP3_RX = 3; // Connects to module's TX
 SoftwareSerial softwareSerial(PIN_MP3_RX, PIN_MP3_TX);
 
 // Create the Player object
-//DFRobotDFPlayerMini player;
+DFRobotDFPlayerMini player;
 
 void setup() {
   // Init USB serial port for debugging
@@ -31,166 +34,84 @@ void setup() {
   softwareSerial.begin(9600);
 
   // Start communication with DFPlayer Mini
-//  if (player.begin(softwareSerial)) {
-   Serial.println("OK");
+  if (player.begin(softwareSerial)) {
+    Serial.println("OK");
 
     // Set volume to maximum (0 to 30).
-   // player.volume(30);
+  // player.volume(30);
     // Play the first MP3 file on the SD card
-   // player.play(1); 
+   // player.play(1);
 
-  pinMode(LED_2, OUTPUT);
-  pinMode(LED, OUTPUT);
-  pinMode(BUTTON, INPUT_PULLUP);
-  pinMode(BUTTON_2, INPUT_PULLUP);
-  strip.begin();
-  strip.show(); // Initialize all pixels to 'off'
+
+    digitalWrite(LED_2, HIGH);
+    digitalWrite(LED, HIGH);
+
+    pinMode(LED_2, OUTPUT);
+    pinMode(LED, OUTPUT);
+    pinMode(BUTTON, INPUT_PULLUP);
+    pinMode(BUTTON_2, INPUT_PULLUP);
+    strip.begin();
+    strip.show(); // Initialize all pixels to 'off'
   }
- 
 
-// *** REPLACE FROM HERE ***
-void loop() { 
+}
+
+
+
+  // *** REPLACE FROM HERE ***
+void loop() {
+
+    unsigned long currentTime = millis();
+
+    bool newState = digitalRead(BUTTON_2);
+    bool newState2 = digitalRead(BUTTON);
 
     // Set volume to maximum (0 to 30).
-    //player.volume(30);
+    player.volume(30);
     // Play the first MP3 file on the SD card
 
-  
-  unsigned long currentTime = millis();
-  digitalWrite(LED_2, HIGH);
-  digitalWrite(LED, HIGH);
-    
-   if (digitalRead(BUTTON_2) == LOW) {
-    //player.play(1);
-      colorWipe(strip.Color(0, 65, 194), 50);
-      colorWipe(strip.Color(60, 125, 254), 50);
-      colorWipe(strip.Color(0, 65, 194), 50);
-   }
-        // blue
-  
-  
-  if  (digitalRead(BUTTON) == LOW) {
-    // player.play(2);
-      colorWipe(strip.Color(255, 165, 0), 50);
-      colorWipe(strip.Color(255, 215, 0), 50);
-      colorWipe(strip.Color(255, 165, 0), 50);
-   }
+    if (digitalRead(BUTTON_2) == LOW) {
+          //colorWipe(strip.Color(0, 65, 194), 50);
+      player.play(2);
+      colorWipe(strip.Color(0, 65, 194), 25);
+      colorWipe(strip.Color(60, 125, 254), 25);
+      colorWipe(strip.Color(0, 65, 194), 25);
 
-   if (currentTime - previousTime >= eventInterval && digitalRead(BUTTON) == HIGH || digitalRead(BUTTON_2) == HIGH) {
-    /* Event code */
-    strip.fill((0,0,0));
+}
+
+
+    if (digitalRead(BUTTON) == LOW) {
+      player.play(1);
+      colorWipe(strip.Color(255, 165, 0), 25);
+      colorWipe(strip.Color(255, 215, 0), 25);
+      colorWipe(strip.Color(255, 165, 0), 25);
+}
+
+    if (currentTime - previousTime >= eventInterval && digitalRead(BUTTON_2) == HIGH) {
+      /* Event code */
+      strip.fill((0, 0, 0));
+      strip.show();
+
+      /* Update the timing for the next time around */
+      previousTime = currentTime;
+}
+
+    if (currentTime - previousTime >= eventInterval && digitalRead(BUTTON) == HIGH) {
+      strip.fill((0, 0, 0));
+      strip.show();
+      digitalWrite(LED_2, LOW);
+
+      previousTime = currentTime;
+    }
+}
+
+void colorWipe(uint32_t c, uint8_t wait) {
+  for(uint16_t i=0; i<strip.numPixels(); i++) {
+    strip.setPixelColor(i, c);
     strip.show();
-    
-   /* Update the timing for the next time around */
-    previousTime = currentTime;
+    delay(wait);
   }
 }
+  
 
-void RunningLights(byte red, byte green, byte blue, int WaveDelay) {
-  int Position=0;
- 
-  for(int j=0; j<NUM_LEDS*2; j++)
-  {
-      Position++; // = 0; //Position + Rate;
-      for(int i=0; i<NUM_LEDS; i++) {
-        // sine wave, 3 offset waves make a rainbow!
-        //float level = sin(i+Position) * 127 + 128;
-        //setPixel(i,level,0,0);
-        //float level = sin(i+Position) * 127 + 128;
-        setPixel(i,((sin(i+Position) * 127 + 128)/255)*red,
-                   ((sin(i+Position) * 127 + 128)/255)*green,
-                   ((sin(i+Position) * 127 + 128)/255)*blue);
-      }
-     
-      showStrip();
-      delay(WaveDelay);
-  }
-}
-
-void RGBLoop(){
-  for(int j = 0; j < 3; j++ ) { 
-    // Fade IN
-    for(int k = 0; k < 256; k++) { 
-      switch(j) { 
-        case 0: setAll(k,0,0); break;
-        case 1: setAll(0,k,0); break;
-        case 2: setAll(0,0,k); break;
-      }
-      showStrip();
-      delay(3);
-    }
-    // Fade OUT
-    for(int k = 255; k >= 0; k--) { 
-      switch(j) { 
-        case 0: setAll(k,0,0); break;
-        case 1: setAll(0,k,0); break;
-        case 2: setAll(0,0,k); break;
-      }
-      showStrip();
-      delay(3);
-    }
-  }
-}
-
-void FadeInOut(byte red, byte green, byte blue){
-  float r, g, b;
-      
-  for(int k = 0; k < 256; k=k+1) { 
-    r = (k/256.0)*red;
-    g = (k/256.0)*green;
-    b = (k/256.0)*blue;
-    setAll(r,g,b);
-    showStrip();
-  }
-     
-  for(int k = 255; k >= 0; k=k-2) {
-    r = (k/256.0)*red;
-    g = (k/256.0)*green;
-    b = (k/256.0)*blue;
-    setAll(r,g,b);
-    showStrip();
-  }
-}
-
-
-// Apply LED color changes
-void showStrip() {
- #ifdef ADAFRUIT_NEOPIXEL_H 
-   // NeoPixel
-   strip.show();
- #endif
- #ifndef ADAFRUIT_NEOPIXEL_H
-   // FastLED
-   FastLED.show();
- #endif
-}
-
-// Set a LED color (not yet visible)
-void setPixel(int Pixel, byte red, byte green, byte blue) {
- #ifdef ADAFRUIT_NEOPIXEL_H 
-   // NeoPixel
-   strip.setPixelColor(Pixel, strip.Color(red, green, blue));
- #endif
- #ifndef ADAFRUIT_NEOPIXEL_H 
-   // FastLED
-   leds[Pixel].r = red;
-   leds[Pixel].g = green;
-   leds[Pixel].b = blue;
- #endif
-}
-
-// Set all LEDs to a given color and apply it (visible)
-void setAll(byte red, byte green, byte blue) {
-  for(int i = 0; i < NUM_LEDS; i++ ) {
-    setPixel(i, red, green, blue); 
-  }
-  showStrip();
-}
-
-void colorWipe(uint32_t color, int wait) {
-  for(int i=0; i<strip.numPixels(); i++) { // For each pixel in strip...
-    strip.setPixelColor(i, color);         //  Set pixel's color (in RAM)
-    strip.show();                          //  Update strip to match
-    delay(wait);                           //  Pause for a moment
-  }
-}
+  
